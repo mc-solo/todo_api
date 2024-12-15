52bca303bb5030ce3b308d6c7616236845d78fc4
Wondwosen <revatoel@gmail.com>
2024-12-15 23:52:16 +0300

    add: function conncetDB to connect to MongoDB

--- server.js
+++ server.js
@@ -5,7 +5,20 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mongo_uri = process.env.MONGO_URI;
 
 app.use(bodyParser.json());
+
+// connect to database
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongo_uri);
+    console.log("connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDb", error);
+  }
+};
+
+connectDB();
 
 app.listen(PORT, () => console.log("app is running on port:", PORT));

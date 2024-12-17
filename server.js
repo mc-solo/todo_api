39a718824686f2ef4bcfbba794a8bd25d0b80bc5
Wondwosen <revatoel@gmail.com>
2024-12-18 00:09:33 +0300

    update: integrate the express router to main server

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+require("./controllers/todoController");
 require("dotenv").config();
+const todoRoutes = require("./routes/todos");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const mongo_uri = process.env.MONGO_URI;
 
+// middleware
 app.use(bodyParser.json());
 app.use(express.json());
+app.use("/api/todos", todoRoutes); //prefix all todo routes with /api/todos
 
 // connect to database
 const connectDB = async () => {
@@ -21,6 +25,7 @@ const connectDB = async () => {
 };
 
 connectDB();
+// console.log(mongo_uri);
 app.get("/", (req, res) => {
   res.send("welcome");
 });
